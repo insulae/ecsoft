6192aa0236ffbe2b7577ac329af4267497a5c704
Martin Alejandro Paz <pazalejandro@gmail.com>
2016-08-04 16:12:11 -0300

    Inicio de Proyecto

---
+++ source/class/ecsoft/theme/Theme.js
@@ -0,0 +1,21 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("ecsoft.theme.Theme",
+{
+  meta :
+  {
+    color : ecsoft.theme.Color,
+    decoration : ecsoft.theme.Decoration,
+    font : ecsoft.theme.Font,
+    icon : qx.theme.icon.Tango,
+    appearance : ecsoft.theme.Appearance
+  }
+});
